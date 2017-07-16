75783168210e579ea9f42a22c8909de9ef883f78
ziyuan <ziyuan@2dfire.com>
2017-07-16 17:01:30 +0800

    节点执行之后  会执行一个钩子方法，用来解耦一些业务无关的操作，比如：发消息

--- src/main/java/com/shiwuliang/BusinessNode.java
+++ src/main/java/com/shiwuliang/BusinessNode.java
@@ -23,6 +23,7 @@ public abstract class BusinessNode {
      */
     public final void handleRequest(RequestContext context, Response response) throws Exception {
         this.handle(context, response);
+        this.hook(context);
         if (this.next != null && !ChannelHolder.isDone()) {
             next.handleRequest(context, response);
         }
@@ -32,4 +33,12 @@ public abstract class BusinessNode {
      * 真正的处理业务
      */
     protected abstract void handle(RequestContext context, Response response) throws Exception;
+
+    /**
+     * 节点之后的钩子
+     * 例子：
+     * 执行节点X之后要发消息，我们就可以解耦发消息的内容，将其放到钩子中
+     */
+    protected void hook(RequestContext context) throws Exception {
+    }
 }
